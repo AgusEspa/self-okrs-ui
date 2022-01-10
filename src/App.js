a8df60b7d0201b1a7c5cea215a0c660656575867
Agustin Espana <agustinespania@gmail.com>
2022-01-10 20:03:55 -0300

    login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import Login from "./components/login";
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div className="App">
-      <p>Hi!</p>
+      <Login 
+        handler={handleLogin}
+        setUsername={setUsername}
+        username={username}
+        setPassword={setPassword}
+        password={password}
+      />
     </div>
   );
 }
