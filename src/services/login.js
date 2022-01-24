82345e1cb7217678e43926935880d5062a7d8953
Agustin Espana <agustinespania@gmail.com>
2022-01-24 13:11:46 -0300

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -1,21 +1,28 @@
+import qs from "qs";
 import axios from 'axios';
 
 const baseUrl = 'http://localhost:8080/login';
 
-const loginService = props => {
+const loginService = (props) => {
+	const credentials = new URLSearchParams();
+	credentials.append('username', props.emailAddress);
+	credentials.append('password', props.password);
 
-	const payload = new URLSearchParams();
-	payload.append('username', props.emailAddress);
-	payload.append('password', props.password);
-
-	try {
-		axios.post(baseUrl, payload);
-		props.setUsername('login');
-	} catch (error) {
-		console.log(`Error: ${error}`)
+	const config = {
+		headers: {
+		  'Content-Type': 'application/x-www-form-urlencoded'
+		}
 	}
 	
-	
+	console.log(qs.stringify(credentials));
+
+	axios.post(baseUrl, qs.stringify(credentials), config)
+  	.then((result) => {
+    	props.setUsername('login')
+  	})
+  	.catch((error) => {
+    	console.log(`Error: ${error}`)
+  	})
 }
 
 export default loginService;
