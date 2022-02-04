29ee960fb22dcb881fba8c11ce359df5c3556fc4
Agustin Espana <agustinespania@gmail.com>
2022-02-04 18:57:17 -0300

    useRefreshToken

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 import jwt_decode from "jwt-decode";
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import useRefreshToken from "./useRefreshToken";
 
 const useAxios = () => {
 
     const { userAuth, setUserAuth, logout } = useContext(AuthContext);
 
+	const refresh = useRefreshToken();
+
 	const baseURL = "http://localhost:8080/api";
-	const refreshURL = `${baseURL}/users/token/refresh`; 
 
 	const axiosInstance = axios.create({
 		baseURL: baseURL,
@@ -16,31 +18,7 @@ const useAxios = () => {
 		headers: { 'Authorization': `Bearer ${userAuth.accessToken}`}
 	});
 
-	const refreshToken = async () => {
-
-		const config = {
-			headers: {
-				'Authorization': `Bearer ${userAuth.refreshToken}`
-			}
-		}
-
-		try {
-			const response = await axios.get(refreshURL, config);
-			
-			window.localStorage.setItem("access_token", response.data.access_token);
-
-			setUserAuth(prevState => ( {
-				username: prevState.username,
-				accessToken: response.data.access_token,
-				refreshToken: prevState.refreshToken
-			}));
-
-		return response.data.access_token;
-		} catch (error) {
-			logout();
-		}
-		
-	}
+	
 
 	axiosInstance.interceptors.request.use(async request => {
 
@@ -52,7 +30,7 @@ const useAxios = () => {
 	
 		if (isValid) return request;
 
-		const refreshedToken = await refreshToken();
+		const refreshedToken = await refresh();
 		
 		request.headers.Authorization = `Bearer ${refreshedToken}`;
 		return request;
@@ -67,10 +45,10 @@ const useAxios = () => {
 
 			if (error.status !== 403) return error;
 	
-			const refreshedToken = await refreshToken();
+			const refreshedToken = await refresh();
 
 			//refactor, cancel before
-			if (refreshToken === undefined) return error;
+			if (refreshedToken === undefined) return error;
 
 			const originalRequest = error.config;
 
---
+++ src/utils/useRefreshToken.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+const useRefreshToken = () => {
+
+	const { userAuth, setUserAuth, logout } = useContext(AuthContext);
+
+	const baseURL = "http://localhost:8080/api";
+	const refreshURL = `${baseURL}/users/token/refresh`;
+
+	const config = {
+		headers: {
+			'Authorization': `Bearer ${userAuth.refreshToken}`
+		}
+	}
+	
+	const refresh = async () => {
+
+		try {
+			
+			const response = await axios.get(refreshURL, config);
+			
+			window.localStorage.setItem("access_token", response.data.access_token);
+
+			setUserAuth(prevState => ( {
+				username: prevState.username,
+				accessToken: response.data.access_token,
+				refreshToken: prevState.refreshToken
+			}));
+
+			return response.data.access_token;
+		
+		} catch (error) {
+			//logout();
+		}
+	}
+
+	return refresh;
+	
+}
+
+export default useRefreshToken;
