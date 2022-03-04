90a29bbf727a72e5ddf7d73cd04a31015df28840
Agustin Espana <agustinespania@gmail.com>
2022-03-04 20:34:18 -0300

    burger menu

--- src/components/Dashboard/Navbar/Navbar.js
+++ src/components/Dashboard/Navbar/Navbar.js
@@ -7,6 +7,13 @@ const NavBar = (props) => {
 
 	const { userAuth, logout } = useContext(AuthContext);
 	const [userMenuDisplay, setUserMenuDisplay] = useState(false);
+	const [responsiveNavDisplay, setResponsiveNavDisplay] = useState(false);
+
+	const handleResponsiveNavToggle = (event) => {
+		event.preventDefault();
+		setResponsiveNavDisplay(prevState => !prevState);
+	}
+	
 
 	return (
 		<nav className="navbar">
@@ -26,7 +33,26 @@ const NavBar = (props) => {
 						{userMenuDisplay && <UserMenu logout={logout} />}
 					</li>
 				</ul>
-				</div>
+			</div>
+
+			<div className="responsive-links-menu">
+				<button onClick={handleResponsiveNavToggle}>X</button>
+				{responsiveNavDisplay &&
+			  	<ul>
+				  	<li>
+					  	<Link to="/dashboard"><span>Dashboard</span></Link>
+						<span>{userAuth.username}</span>
+						<ul>
+							<li>
+							<Link to="/settings"><span>Settings</span></Link>
+							</li>
+							<li>
+							<button onClick={props.logout}><span>Logout</span></button>
+							</li>
+						</ul>
+					</li>
+				</ul>}
+			</div>
 			
     	</nav>
 	);
